cc4b90ab389459cdb59c5c3eb2ddf9321eaa16d6
Brandan Sayarath <brandan@brandan.tech>
2020-05-02 09:21:12 -0700

    add IBOulet from card model class

---
+++ MatchGame/CardCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  CardCollectionViewCell.swift
+//  MatchGame
+//
+//  Created by Brandan Sayarath on 5/2/20.
+//  Copyright © 2020 Brandan Sayarath. All rights reserved.
+//
+
+import UIKit
+
+class CardCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var frontImageView: UIImageView!
+    
+    @IBOutlet weak var backImageView: UIImageView!
+    
+}
--- MatchGame/CardModel.swift
+++ MatchGame/CardModel.swift
@@ -9,30 +9,33 @@
 import Foundation
 
 class CardModel {
-    
+
     func getCards() -> [Card] {
-        
-        //Declare an empty array to store cards
+
         var generatedCards = [Card]()
         var generatedNumber = [Int]()
-        
-//        Randomly generate 8 pairs of cards
+
         while (generatedNumber.count < 8) {
             let randomNumber = Int.random(in: 1...13)
-            
-            if !generatedNumber.contains(randomNumber) {
-                generatedNumber.append(randomNumber)
+
+            if generatedNumber.contains(Int(randomNumber)) == false {
+                generatedNumber.append(Int(randomNumber))
             }
-            
+
             let cardOne = Card()
             let cardTwo = Card()
-            
+
             cardOne.imageName = "card\(randomNumber)"
             cardTwo.imageName = "card\(randomNumber)"
+
+            generatedCards += [cardOne, cardTwo]
+
+            
+            print(randomNumber)
         }
-        
         generatedCards.shuffle()
         
         return generatedCards
     }
 }
+
