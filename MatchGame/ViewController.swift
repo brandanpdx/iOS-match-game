3d24aa8c9283a4bb68b9f6a8c472c2cc4a464e14
Brandan Sayarath <brandan@brandan.tech>
2020-05-02 10:26:14 -0700

    add UICollectionView

--- MatchGame/ViewController.swift
+++ MatchGame/ViewController.swift
@@ -8,7 +8,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
     
     let model = CardModel()
     var cardsArray = [Card]()
@@ -18,8 +21,27 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
         
         cardsArray = model.getCards()
+        
+        //Set view controller as data source and delegate of CollectionView
+        collectionView.dataSource = self
+        collectionView.delegate = self
     }
-
-
+    
+    
+    // MARK: - Collcetion View Delegate Methods
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 16
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        //Get a cell
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CardCell", for: indexPath)
+        
+        // TODO: Finish configuring the cell
+        
+        //Return the cell
+        return cell
+    }
 }
 
